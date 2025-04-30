9f732559ee0f3b4a40eb7508ee71fe2e2369444c
Nikolay Kolev <nbkolev@banchev.bg>
2025-04-30 03:36:02 +0300

    fix: Simple mistake

--- 01-counting/once.py
+++ 01-counting/once.py
@@ -6,6 +6,6 @@ seen_only_once = 0
 for frequency_bin in get_binned_frequencies():
     # As data is binned we know that every bin contains unique keys,
     # we need only to count the numbers where frequency (the value in the dict) = 1
-    seen_only_once += frequency_bin.values().count(1)
+    seen_only_once += list(frequency_bin.values()).count(1)
 
 print(f"{seen_only_once} numbers seen only once")
